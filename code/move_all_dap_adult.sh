11c4293d37b8a1c494f0684d4cfbce84cdf4273a
Pablo Caceres <pcaceres@wisc.edu>
2019-04-10 14:29:29 -0500

    clean files

--- code/move_all_dap_adult.sh
+++
@@ -1,5 +0,0 @@
-mkdir human_drawings_adults
-find  . -name *DAP.png -exec mv -t ./human_drawings_adults/ {} +
-# find n the current directory all the files finishing on "*DAP.png"
-# The execute the mv command to move all the files to the ./human_drawings_adults/ folder
-
